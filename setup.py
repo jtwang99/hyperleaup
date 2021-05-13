d6cf083ef003242157a3503466ed7ba392c7bc1d
JT Wang <jt.wang@auo.com>
2021-05-13 15:18:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hyperleaup',
-    version='0.0.5',
+    version='0.0.6',
     packages=['hyperleaup'],
     url='https://github.com/goodwillpunning/hyperleaup',
     license='None',
